bcf78f687dff5de37216025ee12b5e84d819eaad
LiesbethV <liesbeth.verwerft@student.ehb.be>
2016-10-20 10:48:10 +0100

    test

--- NMBS services/src/be/nmbs/userInterface/Main.java
+++ NMBS services/src/be/nmbs/userInterface/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello NMBS!");
 		System.out.println("Hello NMBS! 2 ");
-		System.out.println("Hello NMBS! 3");
+		System.out.println("test");
 		
 	}
 }
