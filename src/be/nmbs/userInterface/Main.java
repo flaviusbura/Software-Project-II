24049e151a1e1aa5bd11f63c7f49e28b1029583f
glenndelamarche <glenn.de.la.marche@student.ehb.be>
2016-10-24 11:57:31 +0200

    commit op mijn branch

--- src/be/nmbs/userInterface/Main.java
+++ src/be/nmbs/userInterface/Main.java
@@ -3,5 +3,6 @@ package be.nmbs.userInterface;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello mijn branch");
+		System.out.println("Printen op mijn branch");
 	}
 }
