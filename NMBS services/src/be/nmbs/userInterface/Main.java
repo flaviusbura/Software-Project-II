c6721bf7d0930d34cd5f73338740cee89615c920
SamLeirens <leirenssam@gmail.com>
2016-10-24 10:28:03 +0200

    Lmao

--- NMBS services/src/be/nmbs/userInterface/Main.java
+++ NMBS services/src/be/nmbs/userInterface/Main.java
@@ -2,6 +2,6 @@ package be.nmbs.userInterface;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello NMBS!");
+		System.out.println("Hello NMBS!xddddddd");
 	}
 }
