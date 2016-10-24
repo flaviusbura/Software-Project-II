e9a5dfe4f278810ee46d84dbf729ea0f56d15da1
LoStIt <LoStIt@10.3.208.156>
2016-10-24 11:16:49 +0200

    Changes to Persoon

--- NMBS services/src/be/nmbs/logic/Persoon.java
+++ NMBS services/src/be/nmbs/logic/Persoon.java
@@ -4,19 +4,22 @@ public class Persoon {
 	private String voornaam;
 	private String achternaam;
 	private Adres adres;
+	private char geslacht;
 	
 	// Methods
-	public void wijzigAdres(Adres a) {
-		this.adres = a;
+	public void wijzigAdres(Adres adres) {
+		this.adres = adres;
 	}
 	
 	// Getters
 	public String getVoornaam() { return voornaam; }
 	public String getAchternaam() { return achternaam; }
 	public Adres getAdres() { return adres; }
+	public char getGeslacht() { return geslacht; }
 	
 	// Setters
 	public void setVoornaam(String voornaam) { this.voornaam = voornaam; }
 	public void setAchternaam(String achternaam) { this.achternaam = achternaam; }
 	public void setAdres(Adres adres) { this.adres = adres; }
+	public void setGeslacht(char geslacht) { this.geslacht = geslacht; }
 }
