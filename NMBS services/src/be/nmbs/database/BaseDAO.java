b7df39d2cb8f2c4fef18c76041ce6e4827425ce3
SamLeirens <leirenssam@gmail.com>
2016-11-01 15:12:01 +0100

    local

---
+++ NMBS services/src/be/nmbs/database/BaseDAO.java
@@ -0,0 +1,37 @@
+package be.nmbs.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+/**
+ * Deze klasse heeft de connectie met de databank
+ * @author flaviusb
+ *
+ */
+public class BaseDAO {
+	private static Connection connection;
+	/**
+	 * Deze methode stuurt mijn connectie terug
+	 * @return
+	 */
+	protected static Connection getConnection() {
+		return connection;
+	}
+	/**
+	 * Deze methode sets mijn connectie
+	 * @param connection
+	 */
+	protected void setConnection(Connection connection) {
+		BaseDAO.connection = connection;
+	}
+	/**
+	 * Deze constructor vraagt de connectie aan de DatabaseSigleton en initialiseert mijn
+	 * datamember connection van deze klasse 
+	 */
+	public BaseDAO() {
+		try {
+			setConnection(DatabaseSingleton.getDatabaseSingleton().getConnection());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
