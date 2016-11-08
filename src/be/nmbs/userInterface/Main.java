f638086d55ace3518bbe81328e242cc83c569274
Nico <lostitz0r@gmail.com>
2016-11-08 15:01:18 +0100

    cleanup done

--- src/be/nmbs/userInterface/Main.java
+++
@@ -1,8 +0,0 @@
-package be.nmbs.userInterface;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello mijn branch");
-		System.out.println("Printen op mijn branch");
-	}
-}
