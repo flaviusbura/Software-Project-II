a2803d2a54e0d5d3cdf4fd25dda16301270e5461
flaviusb <bura_flavius@yahoo.com>
2016-11-13 14:21:45 +0100

    VerloprenVoorwerp testen in de main + 3 errors gefixed en documentatie.

--- NMBS services/src/be/nmbs/userInterface/Main.java
+++ NMBS services/src/be/nmbs/userInterface/Main.java
@@ -1,8 +1,27 @@
 package be.nmbs.userInterface;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
+import be.nmbs.database.VerlorenVoorwerpenDAO;
+import be.nmbs.logic.VerlorenVoorwerp;
+
 public class Main {
 	public static void main(String[] args) {
+		Date datum = new Date(2016, 11, 13);
+		VerlorenVoorwerp verlorenVoorwerp = new VerlorenVoorwerp(1, "Midi", "MacBook", datum, true);
+		VerlorenVoorwerp verlorenVoorwerp2 = new VerlorenVoorwerp(2, "Gent", "MSI", datum, true);
 		
+		VerlorenVoorwerpenDAO dao = new VerlorenVoorwerpenDAO();
+		
+		//dao.insert(verlorenVoorwerp);
+		//dao.insert(verlorenVoorwerp2);
+		
+		ArrayList<VerlorenVoorwerp> lijst = new ArrayList<VerlorenVoorwerp>();
+		lijst = dao.getAll();
+		
+		for (VerlorenVoorwerp voorwerp : lijst) {
+			System.out.println("Voorwerp: " + voorwerp.getOmschrijving());
+		}
 	}
-
 }
