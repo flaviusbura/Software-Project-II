2a5e676830995b46261d759c64e8397d739a5fba
Nico <lostitz0r@gmail.com>
2016-11-15 12:22:23 +0100

    Fixed errors

--- NMBS services/src/be/nmbs/userInterface/Main.java
+++ NMBS services/src/be/nmbs/userInterface/Main.java
@@ -2,25 +2,6 @@ package be.nmbs.userInterface;
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		
-=======
-		/*Gebruiker gebruiker = new Gebruiker(1, "Flavius", "Bura", "password", 1, true);
-		Gebruiker gebruiker2 = new Gebruiker(2, "User", "User", "User", 1, true);
-	    gebruikerDAO.insert(gebruiker);
-		gebruikerDAO.insert(gebruiker2);*/
-		Gebruiker gebruiker = new Gebruiker(1, "Flavius", "Bura", "veranderd", 1, true);
-		GebruikerDAO gebruikerDAO = new GebruikerDAO();
-		Gebruiker gebruiker2 = new Gebruiker(2, "User", "User", "User", 1, true);
-		gebruikerDAO.delete(gebruiker2);
-		gebruikerDAO.updateWachtwoordById(gebruiker);
-		ArrayList<Gebruiker> gebruikers = new ArrayList<>();
-		gebruikers = gebruikerDAO.getAll();
-		
-		for (Gebruiker g : gebruikers) {
-			System.out.println("Gebruikersnaam: " + g.getAchternaam());
-		}
->>>>>>> refs/heads/Sam
 	}
-
 }
