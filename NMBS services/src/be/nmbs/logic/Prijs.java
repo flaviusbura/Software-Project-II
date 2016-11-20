fab6e7d67921169866185d03909b98e019d4e778
AbdelilahR <abdelilah.razzouk@student.ehb.be>
2016-11-20 22:06:27 +0100

    Junit test voor Prijs,Station en VerlorenVoorwerpen

--- NMBS services/src/be/nmbs/logic/Prijs.java
+++ NMBS services/src/be/nmbs/logic/Prijs.java
@@ -126,4 +126,50 @@ public class Prijs {
 	public void setActief(boolean actief) {
 		this.actief = actief;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (actief ? 1231 : 1237);
+		result = prime * result + ((omschrijving == null) ? 0 : omschrijving.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(prijs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + prijsId;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prijs other = (Prijs) obj;
+		if (actief != other.actief)
+			return false;
+		if (omschrijving == null) {
+			if (other.omschrijving != null)
+				return false;
+		} else if (!omschrijving.equals(other.omschrijving))
+			return false;
+		if (Double.doubleToLongBits(prijs) != Double.doubleToLongBits(other.prijs))
+			return false;
+		if (prijsId != other.prijsId)
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+	
+	
+
+	
 }
