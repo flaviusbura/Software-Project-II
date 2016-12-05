94d1afc0fa14f0fcb7f37128c90741f48558473a
SamLeirens <leirenssam@gmail.com>
2016-12-05 15:01:50 +0100

    replace

--- NMBS services/src/be/nmbs/database/DatabaseSingleton.java
+++ NMBS services/src/be/nmbs/database/DatabaseSingleton.java
@@ -17,7 +17,7 @@ public class DatabaseSingleton {
      * Deze methode gaat indien mijn datamember databaseSingleton initialiseren als deze null is anders
      * stuurt hij de databaseSingleton terug
      * @return
-     */
+     */	
     public static DatabaseSingleton getDatabaseSingleton() {
     	if (databaseSingleton == null) {
     		databaseSingleton = new DatabaseSingleton();
@@ -40,12 +40,20 @@ public class DatabaseSingleton {
                 String username = "SP2G8";
                 String password = "Groep8";
 
-                connection = DriverManager.getConnection(
-                        url, username, password);
+                connection = DriverManager.getConnection(url, username, password);
+              
+        
+                
+                	
             } catch (SQLException e) {
                 System.out.println("SQLException: " + e.getMessage());
                 System.out.println("SQLState: " + e.getSQLState());
                 System.out.println("SQLVendorError: " + e.getErrorCode());
+              	 // db parameters
+                String url1 = "jdbc:sqlite:C:/Users/leire_000/Desktop/NMBSServices.db";
+                // create a connection to the local database
+                connection = DriverManager.getConnection(url1);
+                
             }            
         }
 		return connection;
