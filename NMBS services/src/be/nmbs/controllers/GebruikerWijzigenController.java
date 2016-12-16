f05998e568138fe24dfea5be57ab7313a0a51775
Nico <lostitz0r@gmail.com>
2016-12-16 14:15:08 +0100

    Kleine aanpassing

--- NMBS services/src/be/nmbs/controllers/GebruikerWijzigenController.java
+++ NMBS services/src/be/nmbs/controllers/GebruikerWijzigenController.java
@@ -79,7 +79,7 @@ public class GebruikerWijzigenController {
 
             @Override
             public void changedUpdate(DocumentEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                throw new UnsupportedOperationException("Not supported yet.");
             }
         });
 	}
