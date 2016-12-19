f5745983eff540bfea50c33633b839480bb1c03c
LoStIt <lostitz0r@gmail.com>
2016-12-19 10:08:25 +0100

    quick fix @ homeview

--- NMBS services/src/be/nmbs/userInterface/HomeView.java
+++ NMBS services/src/be/nmbs/userInterface/HomeView.java
@@ -24,6 +24,7 @@ public class HomeView {
 	private static JButton prijzenBeheer;
 	private static JButton btnPush;
 	private static JLabel admin;
+	private static JLabel user;
 	private static JPanel panel;
 	
 	@SuppressWarnings("unused")
@@ -33,96 +34,88 @@ public class HomeView {
 		panel = new JPanel(new GridBagLayout());
 		
 		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
 		
 		if (View.getIngelogdGebruiker().getRol() == 2) {
 			admin = new JLabel("Admin");
-			c.fill = GridBagConstraints.HORIZONTAL;
 			c.insets = new Insets(20, 0, 0, 0);
 			c.gridx = 0;
-			c.gridy = 2;
+			c.gridy = 4;
 			panel.add(admin, c);
 			
 			gebruikersBeheer = new JButton("Gebruikersbeheer");
-			c.fill = GridBagConstraints.HORIZONTAL;
 			c.insets = new Insets(5, 0, 0, 0);
 			c.gridx = 0;
-			c.gridy = 3;
+			c.gridy = 5;
 			panel.add(gebruikersBeheer, c);
 
 			prijzenBeheer = new JButton("Prijzenbeheer");
-			c.fill = GridBagConstraints.HORIZONTAL;
 			c.insets = new Insets(5, 5, 0, 0);
 			c.gridx = 1;
-			c.gridy = 3;
+			c.gridy = 5;
 			panel.add(prijzenBeheer, c);
 			
 			btnStatistieken = new JButton("Statistieken");
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.insets = new Insets(5, 5, 0, 0);
 			c.gridx = 2;
-			c.gridy = 3;
+			c.gridy = 5;
 			panel.add(btnStatistieken, c);
 		}
 
 		btnAbonnement = new JButton("Abonnementen");
-		c.fill = GridBagConstraints.HORIZONTAL;
 		c.insets = new Insets(0, 0, 0, 0);
 		c.gridx = 0;
-		c.gridy = 0;
+		c.gridy = 2;
 		panel.add(btnAbonnement, c);
 
 		btnTicket = new JButton("Tickets");
-		c.fill = GridBagConstraints.HORIZONTAL;
 		c.insets = new Insets(0, 5, 0, 0);
 		c.gridx = 1;
-		c.gridy = 0;
+		c.gridy = 2;
 		panel.add(btnTicket, c);
 
 		btnVerlorenvoorwerpen = new JButton("Verloren voorwerpen");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.insets = new Insets(0, 5, 0, 0);
 		c.gridx = 2;
-		c.gridy = 0;
+		c.gridy = 2;
 		panel.add(btnVerlorenvoorwerpen, c);
 
 		btnBoete = new JButton("Boetes");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.insets = new Insets(0, 5, 0, 0);
 		c.gridx = 3;
-		c.gridy = 0;
+		c.gridy = 2;
 		panel.add(btnBoete, c);
 
 		btnRoutes = new JButton("Routes zoeken");
-		c.fill = GridBagConstraints.HORIZONTAL;
 		c.insets = new Insets(5, 0, 0, 0);
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = 3;
 		panel.add(btnRoutes, c);
 		
 		klantenBeheer = new JButton("Klantenbeheer");
-		c.fill = GridBagConstraints.HORIZONTAL;
 		c.insets = new Insets(5, 5, 0, 0);
 		c.gridx = 1;
-		c.gridy = 1;
+		c.gridy = 3;
 		panel.add(klantenBeheer, c);
 		
-		setBtnTrains(new JButton("Treinen zoeken"));
-		c.fill = new GridBagConstraints().HORIZONTAL;
-		c.gridx = 0;
+		btnTrains = new JButton("Treinen zoeken");
+		c.gridx = 2;
 		c.gridy = 3;
 		panel.add(btnTrains, c);
 		
 		btnPush = new JButton("Offline gegevens doorsturen");
-		c.fill = new GridBagConstraints().HORIZONTAL;
-		c.gridx = 1;
+		c.gridx = 3;
 		c.gridy = 3;
 		panel.add(btnPush, c);
 		
 		logOut = new JButton("Log out");
-		c.fill = new GridBagConstraints().HORIZONTAL;
+		c.insets = new Insets(0, 0, 20, 0);
 		c.gridx = 0;
-		c.gridy = 4;
+		c.gridy = 0;
 		panel.add(logOut, c);
+		
+		user = new JLabel("Opties");
+		c.insets = new Insets(0, 0, 5, 0);
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(user, c);
 		
 		homeController = new HomeController(view);
 		return panel;
@@ -183,5 +176,9 @@ public class HomeView {
 
 	public static void setBtnTrains(JButton btnTrains) {
 		HomeView.btnTrains = btnTrains;
+	}
+	
+	public static void setHomeControllerToNull() {
+		homeController = null;
 	}
 }
