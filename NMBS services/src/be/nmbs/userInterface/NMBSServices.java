c1f88df165e0bab99d0278c346538063854a9bf7
flaviusb <bura_flavius@yahoo.com>
2016-12-30 23:31:10 +0100

    Nimbus look and feel

--- NMBS services/src/be/nmbs/userInterface/NMBSServices.java
+++ NMBS services/src/be/nmbs/userInterface/NMBSServices.java
@@ -2,8 +2,26 @@ package be.nmbs.userInterface;
 
 import java.text.ParseException;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class NMBSServices {
 	public static void main(String[] args) throws ParseException {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+	    } 
+	    catch (UnsupportedLookAndFeelException e) {
+	       
+	    }
+	    catch (ClassNotFoundException e) {
+	       
+	    }
+	    catch (InstantiationException e) {
+	       
+	    }
+	    catch (IllegalAccessException e) {
+	       
+	    }
 		View view = new View();
 		Application application = new Application(view);
 		application.run();
